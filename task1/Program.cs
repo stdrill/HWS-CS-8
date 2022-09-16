ad1c23ce291a1fbe47a73b4d145abbef55c162f3
stdrill <tanya23.96@mail.ru>
2022-09-16 23:24:26 +0300

    initial commit

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void PrintNumbers(int n, int m)
+{
+    if (n < m) return;
+    Console.WriteLine($"{m}");
+    PrintNumbers(n, m+1);
+}
+
+Console.WriteLine("Enter M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+PrintNumbers(numberN,numberM);
