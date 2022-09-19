5888700d8d68a484a4ad8d6d9acce2833c07ad3f
stdrill <tanya23.96@mail.ru>
2022-09-19 22:59:03 +0300

    task2 commit

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumNumbers(int m, int n)
+{
+     if (n == m) return n;
+    return (m + SumNumbers(m + 1, n));
+}
+
+Console.WriteLine("Enter M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{SumNumbers(numberM,numberN)}");
