f055c29c924713f3ae50cc0d99f066645ce5790a
stdrill <tanya23.96@mail.ru>
2022-09-19 23:04:54 +0300

    task3 commit

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int DigitSum(int n)
+{
+    if (n == 0) return 0;
+    return n % 10 + DigitSum(n / 10);
+}
+
+Console.WriteLine("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum of digits {DigitSum(n)}");
