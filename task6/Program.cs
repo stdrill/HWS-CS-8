a7fcf79cd300edab1e079e9e3bff5963e2a9e851
stdrill <tanya23.96@mail.ru>
2022-09-20 23:49:57 +0300

    task6 added

---
+++ task6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+
+void Fibonacci(int a, int b, int n)
+{
+    if (n == 0) return;
+    else {
+        Console.Write((a + b) + " ");
+        Fibonacci(b, a + b, n - 1);
+    }
+}
+
+Console.WriteLine("Enter number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int N = 10;
+Fibonacci(A, B, N);
