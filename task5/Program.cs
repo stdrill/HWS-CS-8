15ae4f742fe70d6bc1c6068c107723ed9ed7a9eb
stdrill <tanya23.96@mail.ru>
2022-09-20 23:08:24 +0300

    task5 added

---
+++ task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+
+int Exponentiation(int a, int b)
+{
+    if (b == 0) return 1;
+    else return Exponentiation(a, b - 1) * a;
+}
+
+Console.Write("Enter number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Exponentiation(a, b));
